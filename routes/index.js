c4854d8900d6da3241f30f1364e959c0c3dd19b0
ubergesundheit <ubergesundheit@googlemail.com>
2014-10-20 22:06:21 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,7 @@
+// Defines the routes and params name that will be passed in req.params
+// routes tell Koop what controller method should handle what request route
+
+module.exports = {
+  // route : handler
+  'get /dwd/:id': 'get'
+}
